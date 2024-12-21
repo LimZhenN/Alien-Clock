46c97bcacfc2c2d75277864b1321d47afd1c591d
LimZhenN <limzn010119@gmail.com>
2024-12-21 19:32:00 +0800

    all

--- obj/Debug/net8.0/Alien Clock.AssemblyInfo.cs
+++ obj/Debug/net8.0/Alien Clock.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Alien Clock")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55bba117b872d44d5337ca051e379adf9d743f5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Alien Clock")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Alien Clock")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
